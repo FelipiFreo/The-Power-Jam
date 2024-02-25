0be1c648025fb110c79dc1140ef523ec9806aec7
Frederico Mendonca Franca <fredfranssa@gmail.com>
2024-02-25 00:27:34 -0300

    Model_Penalties created

--- db/migrate/20240221231119_create_penalties.rb
+++ db/migrate/20240221231119_create_penalties.rb
@@ -1,7 +1,7 @@
 class CreatePenalties < ActiveRecord::Migration[7.1]
   def change
     create_table :penalties do |t|
-      t.string      :name
+      t.string      :penalty_name
       t.string      :penalty_code
       t.string      :description
       t.string      :hand_signal
