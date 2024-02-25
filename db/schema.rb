536a44796b1d60f545d4b9c7863f5bc72600509e
Frederico Mendonca Franca <fredfranssa@gmail.com>
2024-02-24 21:04:53 -0300

    Model League_in_the_Game created

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_232704) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_235204) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "game_name"
+    t.date "game_date"
+    t.time "game_time"
+    t.string "game_location_nickname"
+    t.string "game_map_address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "league_in_the_games", force: :cascade do |t|
+    t.bigint "league_id", null: false
+    t.bigint "game_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_league_in_the_games_on_game_id"
+    t.index ["league_id"], name: "index_league_in_the_games_on_league_id"
+  end
 
   create_table "leagues", force: :cascade do |t|
     t.string "league_name"
@@ -48,4 +67,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_232704) do
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "league_in_the_games", "games"
+  add_foreign_key "league_in_the_games", "leagues"
 end
