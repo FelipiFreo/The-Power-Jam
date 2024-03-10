84edcfe09a97a8124a9a6d7971b961f7024b5b0c
Felipi Freo <PsicoFelipi@gmail.com>
2024-03-10 11:20:24 -0300

    Ajustes

--- db/seeds/players.rb
+++ db/seeds/players.rb
@@ -1,41 +1,43 @@
 def seed_jogadora
 
-  # pseduocodigo
-  # fazer umas variáveis
+# Nome do Modelo
+puts "  Jogadoras".colorize(:light_yellow)
 
 
-  # Nome do Modelo
-  puts "  Jogadoras".colorize(:light_yellow)
 
-  # Destroying
-  puts "  - Destruindo registros existentes (!)".colorize(:light_red)
-  Player.destroy_all
-
-  # Dados
-  puts "  - Preparando dados para criação de novos registros".colorize(:green)
-
-    # Arrays com nomes, números e ligas fictícias
-    nomes = ["Ana", "Beatriz", "Carla", "Daniela", "Eduarda", "Fernanda", "Gabriela", "Helena"]
-    sobrenomes = ["Silva", "Oliveira", "Santos", "Pereira", "Almeida", "Lima", "Martins", "Costa"]
-    derby_names_prontos = ["Blaze Thunderstrike", "Viper Venomous", "Rogue Havoc", "Stinger Frostbite", "Velocity Wildfire", "Thunder Phoenix", "Blaze Cyclone", "Rogue Quicksilver"]
-    numeros_derby = ["101", "202", "303", "404", "505", "606", "707", "808"]
+# Destroying
+puts "  - Destruindo registros existentes (!)".colorize(:light_red)
+Player.destroy_all
 
 
-  # Criação
-  puts "  - Criando #{nomes.size} novas jogadoras".colorize(:green)
 
-    nomes.size.times do |i|
-      Player.create!(
-        calling_name: nomes[i],
-        family_name: sobrenomes[i],
-        derby_name: derby_names_prontos[i],
-        derby_number: numeros_derby[i],
-        # league: ligas.sample, # Seleção randômica de uma liga
-        email: "#{nomes[i].downcase}@spacejammer.com",
-        password: "Space123", # Use password ao invés de encrypted_password
-        password_confirmation: "Space123" # Adicione isso para confirmar a senha
-      )
-    end
+# Dados
+puts "  - Preparando dados para criação de novos registros".colorize(:green)
+
+  # Arrays com nomes, números e ligas fictícias
+  nomes = ["Ana", "Beatriz", "Carla", "Daniela", "Eduarda", "Fernanda", "Gabriela", "Helena"]
+  sobrenomes = ["Silva", "Oliveira", "Santos", "Pereira", "Almeida", "Lima", "Martins", "Costa"]
+  derby_names_prontos = ["Blaze Thunderstrike", "Viper Venomous", "Rogue Havoc", "Stinger Frostbite", "Velocity Wildfire", "Thunder Phoenix", "Blaze Cyclone", "Rogue Quicksilver"]
+  numeros_derby = ["101", "202", "303", "404", "505", "606", "707", "808"]
+
+
+
+# Criação
+puts "  - Criando #{nomes.size} novas jogadoras".colorize(:green)
+
+  nomes.size.times do |i|
+    Player.create!(
+      calling_name: nomes[i],
+      family_name: sobrenomes[i],
+      derby_name: derby_names_prontos[i],
+      derby_number: numeros_derby[i],
+      email: "#{nomes[i].downcase}@spacejammer.com",
+      password: "Space123", # Use password ao invés de encrypted_password
+      password_confirmation: "Space123" # Adicione isso para confirmar a senha
+    )
+  end
+
+
 
   # Finalização do bloco
   puts "  - Seeds de jogadoras criados com sucesso!".colorize(:yellow)
